ee64436225cd75c2efc688aa5c5f6f78b3f5363d
Vladostata15 <119338156+Vladostata15@users.noreply.github.com>
2024-12-04 14:08:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,52 @@ for i in range (5):
     print(a+a)
 if a > 10:
     print(a + b)
+
+stations_in_israel = [
+    "tel aviv savidor center",
+    "jerusalem malha",
+    "haifa center hashmona",
+    "tel aviv hashalom",
+    "tel aviv university",
+    "ashdod ad halom",
+    "afula",
+    "beersheba merkaz",
+    "netanya",
+    "rishon lezion moshe dayan",
+    "hadera west",
+    "herzliya",
+    "rehovot",
+    "binyamina",
+    "nahariya",
+    "modiin",
+    "kiryat shmona",
+    "ramat gan",
+    "beit shemesh",
+    "bat yam yoseftal"
+]
+while True:
+    start = input("You are starting from: ").lower()
+    if start not in stations_in_israel:
+        print("The name of the station is incorrect! Try again!")
+    else:
+        break
+
+while True:
+    finish = input("You are going to: ").lower()
+    if finish not in stations_in_israel:
+        print("The name of the station is incorrect! Try again!")
+    else:
+        break
+
+perStation = 8
+price = abs(stations_in_israel.index(finish) - stations_in_israel.index(start))*perStation
+
+sum = 0
+while sum < price:
+    print("You need to pay more:", price-sum)
+    pay = int(input("Enter the sum: "))
+    sum+=pay
+if sum>price:
+    print("You have back:", sum-price)
+
+print("Have a good trip!")
